22ede8d3af3e66f8089cfa295121b63a76f18aea
14632791 <14632791@qq.com>
2018-01-13 17:22:37 +0800

    正在提交

---
+++ Metro.DynamicModeules.WebApi/Metro.DynamicModeules.WebApi/Controllers/BusinessTablesController.cs
@@ -0,0 +1,19 @@
+﻿using Metro.DynamicModeules.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Metro.DynamicModeules.Service.Base;
+using Metro.DynamicModeules.Service;
+
+namespace Metro.DynamicModeules.WebApi.Controllers
+{
+    public class BusinessTablesController : ApiControllerBase<sys_BusinessTables>
+    {
+        public BusinessTablesController(BusinessTablesService service) : base(service)
+        {
+        }
+    }
+}
