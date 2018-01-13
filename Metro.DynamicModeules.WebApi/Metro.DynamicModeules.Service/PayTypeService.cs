b9b6804db9be3a86a16baeddae489f51e01d4fe2
14632791 <14632791@qq.com>
2018-01-13 17:47:03 +0800

    PayTypeService

---
+++ Metro.DynamicModeules.WebApi/Metro.DynamicModeules.Service/PayTypeService.cs
@@ -0,0 +1,18 @@
+﻿using Metro.DynamicModeules.Models;
+using Metro.DynamicModeules.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.DynamicModeules.Service
+{
+    public class PayTypeService : ServiceBase<tb_PayType>
+    {
+        protected override object[] GetPrimaryKey(tb_PayType entity)
+        {
+            return new object[] { entity.PayType };
+        }
+    }
+}
